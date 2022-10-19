1254daf857bd0fb54b614aeb78bbad1ba60c0710
Luke Kerbs <luke@Lukes-MBP-3.attlocal.net>
2022-10-19 14:27:39 -0700

    upgraded to material design UI. Added description for each example. Upgraded to bootstrap 5

--- app.py
+++ app.py
@@ -1,86 +1,62 @@
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask import Flask, render_template, Response, request, jsonify, redirect, url_for
+from pprint import pprint
 import requests
-import time
 import random
-
-
+import time
 
 app = Flask(__name__, static_folder='static')
 port=8000
 
-# output link for webapp
-print('\n\n🤖 Pi-Vision 🤖\n')
-print('Log in to Pi-Vision. Go to:')
-print('🏠 http://0.0.0.0:'+ str(port) +'/ if local')
-# get the external ip of the raspberry pi
-ip = requests.get('https://api.ipify.org').text
-print('🌎 http://' + ip + ':'+ str(port) +'/ if remote (most users)')
-print('')
+print('\n\n🤖 Basic Flask Example 🤖\n')
+print('View the dashboard. Go to:')
+print(f'🏠 http://0.0.0.0:{port}/ in your browser\n\n')
 
-# - - - - - - - - - - - - - - - LOG IN MANAGEMENT START - - - - - - - - - - - - - - - - - - - -
+# manages dashboard display and form submission
+@app.route('/', methods=["POST","GET"])
+def home():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print('\n🚨 You Just Submitted a Form! 🚨')
+        pprint(data)
+        print("")
+    return render_template('index.html')
 
 # video recording controls continued
 @app.route('/do-something')
 def do_something():
-    print('\n🏁 I just did something!\n')
+    print('\n🏁 You just clicked a button! 🏁\n')
     return ''
 
-# - - - - - - - - - - - APP PAGES START - - - - - - - - - - - - - - - - -
-
-# full robotic arm control panel
-@app.route('/', methods=["POST","GET"])
-#@login_required
-def home():
-    if request.method == "POST":
-        print('')
-        first_name = request.form.get("firstname")
-        print('First Name:', first_name)
-        last_name = request.form.get("lastname")
-        print('Last Name:', last_name)
-        age = request.form.get("age")
-        print('Age:', age)
-        print('')
-
-    return render_template('index.html')
-
-status = 'on'
-@app.route('/get_toggled_status') 
+@app.route('/toggle', methods=['POST']) 
 def toggled_status():
-    global status
-    if status == 'off':
-        status = 'on'
-    else:
-        status = 'off'
-    
-    print('\nToggle Status:', status, '\n')
-    return status
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        if data['status'] == 'true':
+            status = "✅ The toggle is on!"
+        else:
+            status = "🔴 The toggle is off."
+        print("\n", status, "\n")
+        return status
 
-
-
-down = False
-@app.route('/mousedown') 
+@app.route('/mouse_down') 
 def mousedown():
     global down
     down = True
+    print('\n')
     while down:
         print(random.randint(0,10))
         time.sleep(0.01)
+    print('\n')
     return ''
 
-@app.route('/mouseup')
+@app.route('/mouse_up')
 def mouseup():
     global down
     down = False
+    print("\n😮 You let go of the button!\n")
     return ''
 
-
-
-
-# - - - - - - - - - - - APP PAGES END - - - - - - - - - - - - - - - - -
-
-# - - - - - - - - - - RETA APP START - - - - - - - - - - - - - - - -
 if __name__ == '__main__':
     # start the app
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
 
